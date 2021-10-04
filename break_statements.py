0658c1a95b78808707aa2741c1df3d0ab7af84f0
Ubuntu <ubuntu@ip-172-31-68-156.ec2.internal>
2021-10-04 16:19:12 +0000

    break statements

---
+++ break_statements.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+# Created by: Haokai Li
+# Created on: Oct 2021
+# This Program is about break statements
+
+import random
+
+
+def main():
+    # This function is about break statements
+    answer_number = random.randint(0, 9)
+
+    while True:
+        # input
+        user_string = input("Enter a number between 0 - 9: ")
+        print("")
+
+        # process
+        try:
+            user_number = int(user_string)
+            if user_number == answer_number:
+                # output
+                print("You are correctly.")
+                break
+            else:
+                # output
+                print("{} is wrong.".format(user_number))
+                print("")
+        except Exception:
+            # output
+            print("You didn't enter an integer.")
+            break
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
